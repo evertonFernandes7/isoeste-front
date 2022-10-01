f514b6d1ec8340046e2c5bfde4c47d9816664418
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-10-01 12:00:02 -0300

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import MyRoute from './MyRoute';
 import MyTools from '../pages/MyTools';
 
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={MyTools} />
+      <Route path="/">
+        <MyTools />
+      </Route>
     </Switch>
   );
 }
