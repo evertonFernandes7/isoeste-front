da80376658dcc7697d052927a8a727a4da258055
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-10-01 11:54:01 -0300

    styled title

---
+++ src/components/VuttrText/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  h1 {
+    font-size: 2.5rem;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+  }
+
+  p {
+    font-size: 1.6rem;
+    margin-top: 15px;
+  }
+`;
