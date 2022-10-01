e1ee6b426465997319cf2b3a32621e8e52c8c3b2
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-10-01 11:53:33 -0300

    title app

---
+++ src/components/VuttrText/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as C from './styled';
+
+export default function VuttrText() {
+  return (
+    <C.Title>
+      <h1>VUTTR</h1>
+      <p>Very Useful Tools to remember</p>
+    </C.Title>
+  );
+}
